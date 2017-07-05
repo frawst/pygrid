0e0b4d963d5770f6b2676263421982a05af0ed4a
frawst <frawst@live.ca>
2017-07-05 08:18:32 -0600

    v0.0.2 added BOSS case

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import time
 
 # Player Shit
 # 0x, 1y, 2health, 3pots, 4upgrades
-Player = [0, 0, 30, 3, 0]
+Player = [5, 0, 30000, 3, 0]
 
 # xy, status (0 = empty, 1 = stuff)
 RoomData = {
-	(0,0): 0
+	(5,0): 0,
+	(5,11): 2
 }
 
 play = True
+timer = 1
 
 print('Type \'help\' at any time for assistance')
 print('You enter a room. In front of you is a door.')
@@ -108,7 +110,7 @@ while(play):
 			else:
 				print('Your attack was blocked!')
 
-			time.sleep(1)
+			time.sleep(timer*1)
 
 			if (monhp > 0):
 				mroll = random(1,8)
@@ -122,11 +124,11 @@ while(play):
 				else:
 					print('You blocked the monster\'s attack!')
 
-				time.sleep(1)
+				time.sleep(timer*1)
 
 			if (monhp <= 0):
 				print('You killed the monster and search the room for loot...')
-				time.sleep(3)
+				time.sleep(timer*3)
 				if random(0,2) == 1:
 					print('You found a potion!')
 					Player[3] += 1
@@ -138,6 +140,59 @@ while(play):
 		if (Player[2] <= 0):
 			print('You died, lil\' bitch!')
 			play = False
+
+	if roomstate == 2:
+		print('encountering a BOSS!')
+		input('Press Enter to Combat!')
+		monhp = 50
+		while (monhp > 0 and Player[2] > 0):
+			#print('Rolling for attack! (1d10)')
+			proll = random(1,10)
+			#print('You rolled ', proll)
+			#print('Monster rolling block! (1d8)')
+			mroll = random(1,12)
+			#print('Monster rolled ', mroll)
+			if (proll > mroll):
+				damage = random(1,10)
+				if (damage > monhp):
+					damage = monhp
+				print('Your attack hits dealing ', damage, ' damage! (1d10)')
+				monhp = monhp - damage
+			else:
+				print('Your attack was blocked!')
+
+			time.sleep(timer*1)
+
+			if (monhp > 0):
+				mroll = random(1,12)
+				proll = random(1,10)
+				if (mroll > proll):
+					damage = random(1,12)
+					if (damage > Player[2]):
+						damage = Player[2]
+					print('BOSS attack hits dealing ', damage, 'damage! (1d8)')
+					Player[2] = Player[2] - damage
+				else:
+					print('You blocked the monster\'s attack!')
+
+				time.sleep(timer*1)
+
+			if (monhp <= 0):
+				print('You killed the BOSS and search the room for loot...')
+				time.sleep(timer*3)
+				if random(0,2) == 1:
+					print('You found a potion!')
+					Player[3] += 1
+				else:
+					print('No loot was found.')
+			if (Player[2] <= 0):
+				print('The BOSS fucked you up!')
+		updateroom(Player, RoomData)
+		if (Player[2] <= 0):
+			print('You died, lil\' bitch!')
+			play = False
+
+
 
 
 
