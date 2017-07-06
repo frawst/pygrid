7745093533bc47ea5a8935b56c66b2897f55e78a
frawst <frawst@live.ca>
2017-07-06 08:37:43 -0600

    0.0.4

--- main.py
+++ main.py
@@ -27,6 +27,17 @@ changelog
 		Can now build multiple levels in unique ways
 		Added stat upgrades chance to drop from monster fights
 
+	0.0.4
+		4 Levels
+		Unique bosses on each floor
+		Expanded player stats
+		Loot reworked
+			- added stat gems
+			- increased potion drop rate
+		Boss doors now glow
+		Difficulty now flexes with progression
+		Added MAP functionality
+
 """
 # Global Imports
 from random import randrange as random
@@ -44,9 +55,13 @@ playposy = 0
 playhp = 30
 maxhp = 50
 playpots = 3
+
 playatk = 10
 playdef = 10
 playdmg = 10
+playaccboost = 0
+playdmgboost = 0
+
 slain = 0
 bosses = 0
 
@@ -87,8 +102,6 @@ def drawMap(zone, playx, playy):
 					#print(line)
 				elif (x+1,y+1) == tup:
 					line.append('*')
-				elif zone[x+1,y+1] == 99:
-					line.append('B')
 				else:
 					line.append('#')
 					#print(line)
@@ -104,6 +117,12 @@ play = True
 print('Type \'help\' at any time for help.')
 while (play):
 	zone = generateZone(levels[level])
+	if level == 1:
+		print('You enter the dungeon.')
+	else:
+		print('You descend deeper into the dungeon.')
+	bossmod = zone['bossmod']
+	bossname = zone['bossname']
 	playposx = 1
 	playposy = 1
 	zone[(playposx,playposy)] = 0
@@ -123,9 +142,12 @@ while (play):
 				play = False
 			elif command == 'kill':
 				playhp = 1
+			elif command == 'godmode':
+				maxhp += 1000
+				playhp = maxhp
 			elif command == 'stats':
-				print('Health: %i\nMax HP: %i\nAttack: %i\nDefence: %i\nPotions: %i' %
-					(playhp, maxhp, playatk, playdef, playpots))
+				print('Health: %i\nMax HP: %i\nAttack: %i\nDefence: %i\nPotions: %i\nBonuses: +%i Accuracy, +%i Damage' %
+					(playhp, maxhp, playatk, playdef, playpots, playaccboost, playdmgboost))
 			elif command == 'map':
 				drawMap(zone, playposx, playposy)
 			elif command == 'up':
@@ -181,9 +203,9 @@ while (play):
 
 		except:
 			roomgen = random(6)
-			if roomgen == 0 or roomgen == 1:
+			if roomgen == 0 or roomgen == 1 or roomgen == 2:
 				state = 2
-			elif roomgen == 2 or roomgen == 3 or roomgen == 4:
+			elif roomgen == 3 or roomgen == 4:
 				state = 0
 			else:
 				state = 1
@@ -198,39 +220,62 @@ while (play):
 			elif state == 1:
 				zone[(playposx,playposy)] = 0
 				print('Upon entering the room you discover a small chest.')
-				loot = random(9)
+				loot = random(21)
 				waiting(3)
-				if loot == 0 or loot == 1 or loot == 2:
+				if (loot == 0 or loot == 1 or loot == 2 or loot == 3
+				or loot == 4 or loot == 5 or loot == 6 or loot == 7):
 					print('The chest contained a health potion!')
 					playpots += 1
-				elif loot == 3:
+				elif loot == 8 or loot == 9:
 					print('As you open the chest a surge of glowing red energy flows into your chest.')
 					print('Your health was restored by 30 points!')
 					playhp += 30
+				elif loot == 10 or loot == 11:
+					print('You find an upgrade to your equipment...')
+					loot2 = random(2)
+					time.sleep(1)
+					if loot2 == 0:
+						print('Your defence has increased!')
+						playdef += 1
+					else:
+						print('Your attack has increased!')
+						playatk += 1
+				elif loot == 12:
+					print('You find a skill crystal, and consume it.')
+					loot2 = random(2)
+					time.sleep(1)
+					if loot2 == 0:
+						print('Your damage is permanently increased!')
+						playdmgboost += 1
+					else:
+						print('Your accuracy is permanently increased!')
+						playaccboost += 1
 				else:
 					print('The chest was empty.')
 			elif state == 2:
 				zone[(playposx,playposy)] = 0
-				print('As you enter the room the door shuts behind you and you discover a horrifying creature!')
-				monhp = random(5,21)
-				mondmg = random(3,11)
-				mondice = random(6,12)
-				diff = int((monhp + mondmg + mondice) / 10)
-				if diff == 1:
-					print('It\'s a giant rat!')
-				elif diff == 2:
-					print('It\'s a goblin!')
-				elif diff == 3:
-					print('It\'s a troll!')
-				else:
-					print('IT\'S A DRAGON! The Door is SHUT!')
+				print('As you enter the room the door shuts behind you and you discover a creature!')
+				monhp = random(4,20) + (bossmod - 2) * 10
+				mondmg = random(5,10) + (bossmod - 2)
+				mondice = random(6,11) + (bossmod - 2)
+				# diff = int((monhp + mondmg + mondice) / 10 * (bossmod - 2))
+				# if diff == 1:
+				# 	print('It\'s a fluffy little bunny, aww!')
+				# if diff == 2:
+				# 	print('It\'s a giant rat!')
+				# elif diff == 3:
+				# 	print('It\'s a goblin!')
+				# elif diff == 4:
+				# 	print('It\'s a troll!')
+				# else:
+				# 	print('IT\'S A DRAGON! The Door is SHUT!')
 
 				#Combat Loop
 				while (monhp > 0 and playhp > 0):
 					if monhp > 0 and playhp > 0:
 						time.sleep(1)
-						if random(1,playatk) > random(1,mondice):
-							hit = random(1,playdmg)
+						if random(1,playatk) + playaccboost > random(1,mondice):
+							hit = random(1,playdmg) + playdmgboost
 							print ('>> hit for %i damage' 
 								% hit)
 							monhp -= hit
@@ -239,7 +284,7 @@ while (play):
 					if monhp > 0 and playhp > 0:
 						time.sleep(1)
 						if random(1,mondice) > random(1, playdef):
-							hit = random(1,mondmg)
+							hit = random(1,mondmg) + (int(bossmod/2 - 0.5))
 							print ('<< hit for %i damage.'
 								% hit)
 							playhp -= hit
@@ -251,9 +296,19 @@ while (play):
 					slain += 1
 					waiting(5)
 					loot = random(101)
-					if loot > 74:
+					if loot > 50:
 						print('You find a health potion.')
 						playpots += 1
+					elif loot > 10 and loot < 20:
+						print('You find a skill crystal, and consume it.')
+						loot2 = random(2)
+						time.sleep(1)
+						if loot2 == 0:
+							print('Your damage is permanently increased!')
+							playdmgboost += 1
+						else:
+							print('Your accuracy is permanently increased!')
+							playaccboost += 1
 					elif loot > 0 and loot < 10:
 						print('You find an upgrade to your equipment...')
 						loot2 = random(2)
@@ -273,15 +328,15 @@ while (play):
 				zone[(playposx,playposy)] = 0
 				print('As you enter the room a cold chill runs up your spine.')
 				time.sleep(3)
-				print('A great beast enters.')
-				monhp = 75
-				mondice = 12
+				print('%s enters from the shadows.' % bossname)
+				monhp = 15*bossmod
+				mondice = 7+bossmod
 
 				while (monhp > 0 and playhp > 0):
 					if monhp > 0 and playhp > 0:
 						time.sleep(1)
-						if random(1,playatk) > random(1,mondice):
-							hit = random(1,playdmg)
+						if random(1,playatk) + playaccboost > random(1,mondice):
+							hit = random(1,playdmg) + playdmgboost
 							print ('>> hit for %i damage' 
 								% hit)
 							monhp -= hit
@@ -297,12 +352,38 @@ while (play):
 						else:
 							print ('<< blocked.')
 				if monhp <= 0:
-					print('You have slaughtered the BOSS! A great energy overwhelms you.')
+					print('You have conquered %s! A great energy overwhelms you.'
+						% bossname)
 					print('Stats increased. HP restored to max.')
 					maxhp += 10
 					playhp = maxhp
 					playdef += 2
 					playatk += 2
+					playdmgboost += 2
+					bosskilled = True
+					level += 1
+
+			try:
+				if zone[(playposx+1,playposy)] == 99:
+					print('The door to the east glows with a bloody aura.')
+			except:
+				pass
+			try:
+				if zone[(playposx-1,playposy)] == 99:
+					print('The door to the west glows with a bloody aura.')
+			except:
+				pass
+			try:
+				if zone[(playposx,playposy+1)] == 99:
+					print('The door to the north glows with a bloody aura.')
+			except:
+				pass
+			try:
+				if zone[(playposx,playposy-1)] == 99:
+					print('The door to the south glows with a bloody aura.')
+			except:
+				pass
+				
 
 				if playhp <= 0:
 					play = False
@@ -314,6 +395,6 @@ while (play):
 
 #ENDGAME
 print('Your mission is over. Your final stats were:')
-print('Attack: %i\nDefence: %i\nPotions: %i\nMonsters Slain: %i' %
-	(playatk, playdef, playpots, slain))
+print('Attack: %i\nDefence: %i\nPotions: %i\nMonsters Slain: %i\nBonuses: +%i Accuracy, +%i Damage' %
+	(playatk, playdef, playpots, slain, playaccboost, playdmgboost))
 
--- pygridlevels.py
+++ pygridlevels.py
@@ -1,6 +1,26 @@
 levels = {
 	1:{
 		'grid': [5, 6],
+		'bossmod': 3,
+		'bossname': 'Shakir the Timid',
 		(3,6): 99
+	},
+	2:{
+		'grid': [7,7],
+		'bossmod': 5,
+		'bossname': 'Alfgard, Guardian of the Well',
+		(7,6): 99
+	},
+	3:{
+		'grid': [8,8],
+		'bossmod': 10,
+		'bossname': 'Darian the Malificent',
+		(5,4): 99
+	},
+	4:{
+		'grid': [10,10],
+		'bossmod': 15,
+		'bossname': 'Xeno, Destroyer of Worlds',
+		(8,2): 99
 	},
 }
